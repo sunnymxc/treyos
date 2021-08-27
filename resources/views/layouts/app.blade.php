d6bc87fe990013cdfcc4171ac75b189dfc781d3d
Sifon Isaac <syfonisaac@gmail.com>
2021-08-27 09:47:47 +0100

    home page and booking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,88 +26,129 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md  fixed-top d-flex align-items-center header-transparent" id="header">
-            <div class="container">
+        <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+            <div class="container d-flex align-items-center">
+        
+              <div class="logo mr-auto">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{-- {{ config('app.name', 'Treyos Logo') }} --}}
                     Treyos Logo
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+              </div>
+        
+              <nav class="nav-menu d-none d-lg-block">
+                <ul>
+                  <li class="active"><a href="/">Home</a></li>
+                  <li><a href="#about">About</a></li>
+                  <li><a href="#details">Services</a></li>
+                  <li><a href="#features">Features</a></li>
+                  <li><a class="" href="#agents">Become an Agent</a></li>
+                  
+                   <!-- Authentication Links -->
+                   @guest
+                   @if (Route::has('login'))
+                       <li class="nav-item">
+                           <a class="" href="{{ route('login') }}">{{ __('Login') }}</a>
+                       </li>
+                   @endif
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                   @if (Route::has('register'))
+                       <li class="nav-item">
+                           <a class="" href="{{ route('register') }}">{{ __('Register') }}</a>
+                       </li>
+                   @endif
+               @else
+                   <li class="nav-item dropdown">
+                       <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                           {{ Auth::user()->name }}
+                       </a>
 
-                    </ul>
+                       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                           <a class="dropdown-item" href="{{ route('logout') }}"
+                              onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                               {{ __('Logout') }}
+                           </a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">Home</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#about">About Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#features">Features</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#details">Services</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link btn btn-success" href="#agents">Become an Agent</a>
-                        </li>
-
-
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                           <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                               @csrf
+                           </form>
+                       </div>
+                   </li>
+               @endguest
+                 
+                </ul>
+              </nav><!-- .nav-menu -->
+        
             </div>
-        </nav>
+          </header><!-- End Header -->
+        
 
+
+      
         <main class="">
             @yield('content')
         </main>
+        <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-5 col-md-6">
+            <div class="footer-info">
+              <h3>Treyos</h3>
+              <p class="pb-3"><em>Best Way Home</em></p>
+              <p>
+                23 Ikorodu Road <br>
+                Ikorodu, Lagos<br><br>
+                <strong>Phone:</strong> +23480832902<br>
+                <strong>Email:</strong> info@treyos.com<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-1"></div>
+          <div class="col-lg-2 col-md-6 footer-links ">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="/">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#about">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#details">Services</a></li>
+              {{-- <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li> --}}
+              {{-- <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li> --}}
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Our Newsletter</h4>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+  <div id="preloader"></div>
+
     </div>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('vendor/jquery.easing/jquery.easing.min.js') }}"></script>
     <script src="{{ asset('vendor/venobox/venobox.min.js') }}"></script>
     <script src="{{ asset('vendor/waypoints/jquery.waypoints.min.js') }}"></script>
     <script src="{{ asset('vendor/counterup/counterup.min.js') }}"></script>
