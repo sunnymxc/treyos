173923a04c0c81cc2122350c2c5d1898f5b9f348
Sifon Isaac <syfonisaac@gmail.com>
2021-09-14 17:58:17 +0100

    multi step form

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -6,7 +6,7 @@
 
 <x-jet-validation-errors class="mb-4" />
 
-<section class="book" id="hero">
+<section class="book" id="herro">
 	<div class="container">
 		<form method="post" action="{{ route('booking.store') }}">
 			@csrf
@@ -88,5 +88,169 @@
 			</div>
 		</div>
 	</div>  
+
+	<section class="signup-step-container">
+		<div class="container col-md-8 mx-auto">
+				<div class="row d-flex justify-content-center card card-body shadow p-5 border-0">
+						<div class="col">
+								<div class="wizard">
+										<div class="wizard-inner">
+												<div class="connecting-line"></div>
+												<ul class="nav nav-tabs" role="tablist">
+														<li role="presentation" class="active">
+																<a href="#step1" data-toggle="tab" aria-controls="step1" role="tab" aria-expanded="true"><span class="round-tab">1 </span> <i>Step 1</i></a>
+														</li>
+														<li role="presentation" class="disabled">
+																<a href="#step2" data-toggle="tab" aria-controls="step2" role="tab" aria-expanded="false"><span class="round-tab">2</span> <i>Step 2</i></a>
+														</li>
+														<li role="presentation" class="disabled">
+																<a href="#step3" data-toggle="tab" aria-controls="step3" role="tab"><span class="round-tab">3</span> <i>Step 3</i></a>
+														</li>
+														
+												</ul>
+										</div>
+		
+										<form role="form" action="index.html" class="login-box">
+												<div class="tab-content" id="main_form">
+														<div class="tab-pane active" role="tabpanel" id="step1">
+																<h4 class="text-center mb-4">Step 1</h4>
+																<div class="row">
+																		<div class="col-md-6">
+																				<div class="form-group">
+																						<label>First and Last Name *</label> 
+																						<input class="form-control" type="text" name="name" placeholder=""> 
+																				</div>
+																		</div>
+																		<div class="col-md-6">
+																				<div class="form-group">
+																						<label>Phone Number  *</label> 
+																						<input class="form-control" type="text" name="name" placeholder=""> 
+																				</div>
+																		</div>
+																		<div class="col-md-6">
+																				<div class="form-group">
+																						<label>Email Address *</label> 
+																						<input class="form-control" type="email" name="name" placeholder=""> 
+																				</div>
+																		</div>
+																		
+																		<div class="col-md-6">
+																				<div class="form-group">
+																						<label>Password *</label> 
+																						<input class="form-control" type="password" name="name" placeholder=""> 
+																				</div>
+																		</div>
+																		
+																		
+																</div>
+																<ul class="list-inline pull-right">
+																		<li><button type="button" class="default-btn next-step btn-success">Continue to next step</button></li>
+																</ul>
+														</div>
+														<div class="tab-pane" role="tabpanel" id="step2">
+																<h4 class="text-center mb-4">Step 2</h4>
+																<div class="row">
+																<div class="col-md-6">
+																		<div class="form-group">
+																				<label>Address 1 *</label> 
+																				<input class="form-control" type="text" name="name" placeholder=""> 
+																		</div>
+																</div>
+																
+																<div class="col-md-6">
+																		<div class="form-group">
+																				<label>City / Town *</label> 
+																				<input class="form-control" type="text" name="name" placeholder=""> 
+																		</div>
+																</div>
+																<div class="col-md-6">
+																		<div class="form-group">
+																				<label>Country *</label> 
+																				<select name="country" class="form-control" id="country">
+																						<option value="NG" selected="selected">Nigeria</option>
+																						<option value="NU">Niue</option>
+																						<option value="NF">Norfolk Island</option>
+																						<option value="KP">North Korea</option>
+																						<option value="MP">Northern Mariana Islands</option>
+																						<option value="NO">Norway</option>
+																				</select>
+																		</div>
+																</div>
+																
+																
+																
+																<div class="col-md-6">
+																		<div class="form-group">
+																				<label>Registration No.</label> 
+																				<input class="form-control" type="text" name="name" placeholder=""> 
+																		</div>
+																</div>
+															 </div>
+																
+																
+																<ul class="list-inline pull-right">
+																		<li><button type="button" class="default-btn prev-step btn-secondary">Back</button></li>
+																		<li><button type="button" class="default-btn next-step skip-btn">Skip</button></li>
+																		<li><button type="button" class="default-btn next-step btn-success">Continue</button></li>
+																</ul>
+														</div>
+														<div class="tab-pane" role="tabpanel" id="step3">
+																<h4 class="text-center mb-4">Step 3</h4>
+																 <div class="row">
+																<div class="col-md-6">
+																		<div class="form-group">
+																				<label>Account Name *</label> 
+																				<input class="form-control" type="text" name="name" placeholder=""> 
+																		</div>
+																</div>
+																<div class="col-md-6">
+																		<div class="form-group">
+																				<label>Demo</label> 
+																				<input class="form-control" type="text" name="name" placeholder=""> 
+																		</div>
+																</div>
+																<div class="col-md-6">
+																		<div class="form-group">
+																				<label>Inout</label> 
+																				<input class="form-control" type="text" name="name" placeholder=""> 
+																		</div>
+																</div>
+																<div class="col-md-6">
+																		<div class="form-group">
+																				<label>Information</label> 
+																				<div class="custom-file">
+																					<input type="file" class="custom-file-input" id="customFile">
+																					<label class="custom-file-label" for="customFile">Select file</label>
+																				</div>
+																		</div>
+																</div>
+																<div class="col-md-6">
+																		<div class="form-group">
+																				<label>Number *</label> 
+																				<input class="form-control" type="text" name="name" placeholder=""> 
+																		</div>
+																</div>
+																<div class="col-md-6">
+																		<div class="form-group">
+																				<label>Input Number</label> 
+																				<input class="form-control" type="text" name="name" placeholder=""> 
+																		</div>
+																</div>
+																	 </div>
+																	 <ul class="list-inline pull-right">
+																		<li><button type="button" class="default-btn prev-step btn-secondary">Back</button></li>
+																		<li><button type="button" class="default-btn next-step btn-success">Finish</button></li>
+																</ul>
+														</div>
+													
+														<div class="clearfix"></div>
+												</div>
+												
+										</form>
+								</div>
+						</div>
+				</div>
+		</div>
+</section>
 </section>  
 @endsection
